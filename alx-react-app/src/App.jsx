b28a12947dc7a0be2f2add3342fba8033d6ee4c1
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2025-11-01 00:24:06 +0100

    Add new features

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,21 @@
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <WelcomeMessage />
+     <Header />
+     <MainContent />
+     <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
