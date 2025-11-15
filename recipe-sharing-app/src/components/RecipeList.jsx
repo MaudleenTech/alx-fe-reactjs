70414468dab3f36271e56b7fac0208d7c541cebb
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2025-11-15 02:28:24 +0100

    Task 1: Added recipe details, edit, delete functionality with routing

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import { useRecipeStore } from "./recipeStore";
+import { Link } from "react-router-dom";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
@@ -9,8 +10,15 @@ const RecipeList = () => {
       {recipes.length === 0 && <p>No recipes yet.</p>}
 
       {recipes.map((recipe) => (
-        <div key={recipe.id} style={{ border: "1px solid #ddd", padding: "10px", marginBottom: "10px" }}>
-          <h3>{recipe.title}</h3>
+        <div
+          key={recipe.id}
+          style={{ border: "1px solid #ddd", padding: "10px", marginBottom: "10px" }}
+        >
+          <h3>
+            <Link to={`/recipe/${recipe.id}`} style={{ textDecoration: "none", color: "#333" }}>
+              {recipe.title}
+            </Link>
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -8,5 +8,17 @@ export const useRecipeStore = create((set) => ({
       recipes: [...state.recipes, newRecipe],
     })),
 
+    updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+    
   setRecipes: (recipes) => set({ recipes }),
 }));
