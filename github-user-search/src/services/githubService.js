3285cdfaf977206a9683e00565e6b744c6627a50
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2025-11-22 16:36:35 +0100

    Fixed the search issue

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-// Existing single-user fetch
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
-};
-
 // Advanced search for Task 2
-export const searchUsers = async (username, location = "", minRepos = "") => {
+export const fetchUserData = async (username, location = "", minRepos = "") => {
   try {
     let query = `${username}`;
     if (location) query += `+location:${location}`;
