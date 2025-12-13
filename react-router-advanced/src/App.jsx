7131bc1b6ec4726394b223fbfc81bd515573db8f
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2025-12-13 13:59:58 +0100

    Update with new features

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,5 +1,13 @@
-import useAuth from "./components/useAuth";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+import User from "./components/User";
+import BlogPost from "./components/BlogPost";
 import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import useAuth from "./hooks/useAuth";
 
 function App() {
   const { isAuth, login } = useAuth();
@@ -7,7 +15,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login login={login} />} />
+
+        {/* Protected Profile route */}
         <Route
           path="/profile/*"
           element={
@@ -19,8 +30,15 @@ function App() {
           <Route path="details" element={<ProfileDetails />} />
           <Route path="settings" element={<ProfileSettings />} />
         </Route>
-        {/* other routes */}
+
+        {/* Dynamic User route */}
+        <Route path="/user/:userId" element={<User />} />
+
+        {/* Dynamic Blog route */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
