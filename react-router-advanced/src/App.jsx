211f22dc641d1221b1af902ff2e85ffd42a66c57
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2025-12-13 13:41:50 +0100

    Fix for ALX checker

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 import Home from "./components/Home";
@@ -11,32 +11,32 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
-
   const login = () => setIsAuth(true);
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login login={login} />} />
 
-      <Route path="/login" element={<Login login={login} />} />
+        {/* Protected Profile route */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuth={isAuth}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          {/* Nested routes */}
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
 
-      {/* Protected Profile route */}
-      <Route
-        path="/profile/*"
-        element={
-          <ProtectedRoute isAuth={isAuth}>
-            <Profile />
-          </ProtectedRoute>
-        }
-      >
-        {/* Nested routes */}
-        <Route path="details" element={<ProfileDetails />} />
-        <Route path="settings" element={<ProfileSettings />} />
-      </Route>
-
-      {/* Dynamic User route */}
-      <Route path="/user/:userId" element={<User />} />
-    </Routes>
+        {/* Dynamic User route */}
+        <Route path="/user/:userId" element={<User />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
